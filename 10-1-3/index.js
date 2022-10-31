a8c13fbe85dba139d950fc2ed6a0c4d3d56c231a
김영민 <rpan0522@gmail.com>
2022-10-31 16:15:53 +0900

    실습일지

---
+++ 10-1-3/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PageColor from './chapter12/PageColor';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <PageColor />
+);
