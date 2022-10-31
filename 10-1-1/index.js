93274856fa048421d41eab5b98450c797d7180f6
aripoi <106456245+aripoi@users.noreply.github.com>
2022-10-31 16:20:18 +0900

    Delete 10-1-1 directory

--- 10-1-1/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ContextApp from './chapter12/ContextApp';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <ContextApp />
-);
